6b576264b9e48a75b264a578e13aad7c1777c157
Lim ji yang <fluffy8854@naver.com>
2023-05-16 14:22:16 +0900

    feat : build

---
+++ src/main/java/com/example/sprintbootdockerex/SprintbootDockerExApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprintbootdockerex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootDockerExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintbootDockerExApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sprintbootdockerex/SprintbootDockerExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprintbootdockerex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintbootDockerExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
