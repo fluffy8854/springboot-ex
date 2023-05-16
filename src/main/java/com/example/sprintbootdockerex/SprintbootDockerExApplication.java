a1213795db6626dc79b16e90549d11b9b04f8830
Lim ji yang <fluffy8854@naver.com>
2023-05-16 15:06:53 +0900

    feat : hello home add

--- src/main/java/com/example/sprintbootdockerex/SprintbootDockerExApplication.java
+++ src/main/java/com/example/sprintbootdockerex/SprintbootDockerExApplication.java
@@ -2,10 +2,17 @@ package com.example.sprintbootdockerex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SprintbootDockerExApplication {
 
+    @RequestMapping("/")
+    String home(){
+        return "hello";
+    }
     public static void main(String[] args) {
         SpringApplication.run(SprintbootDockerExApplication.class, args);
     }
